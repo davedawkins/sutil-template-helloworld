f32fd7a7a36798bb414e138d918d02b9afd25de7
David Dawkins <david.dawkins@gmail.com>
2021-02-21 23:53:04 +0000

    first commit

---
+++ src/App/App.fs
@@ -0,0 +1,18 @@
+module App
+
+open Sutil
+open Sutil.DOM
+open Sutil.Attr
+
+let view() =
+    Html.div [
+        style [
+            Css.fontFamily "Arial, Helvetica,sans-serif"
+            Css.textAlign "center"
+            Css.marginTop "40px"
+            Css.fontSize "10ex"
+        ]
+        text "Hello World"
+    ]
+
+mountElement "sutil-app" (view())
