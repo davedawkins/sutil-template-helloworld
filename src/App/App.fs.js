54c91825f4c8de2ddaa461f9b00e3902291d858c
David Dawkins <david.dawkins@gmail.com>
2021-02-22 00:43:33 +0000

    Delete App.fs.js

--- src/App/App.fs.js
+++
@@ -1,7 +0,0 @@
-
-export function view() {
-    return Sutil_DOM_Html_div([Sutil_Attr_style([Sutil_Attr_CssEngine_CssEngine$1__fontFamily_4E60E31B(Sutil_Attr_Css, "Arial, Helvetica,sans-serif"), Sutil_Attr_CssEngine_CssEngine$1__textAlign_4E60E31B(Sutil_Attr_Css, "center"), Sutil_Attr_CssEngine_CssEngine$1__marginTop_4E60E31B(Sutil_Attr_Css, "40px"), Sutil_Attr_CssEngine_CssEngine$1__fontSize_4E60E31B(Sutil_Attr_Css, "10ex")]), Sutil_DOM_text("Hello World")]);
-}
-
-Sutil_DOM_mountElement("sutil-app", view());
-
