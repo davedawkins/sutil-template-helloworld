fb012dd93756348c8b43925aeb5458150ada301d
David Dawkins <david.dawkins@gmail.com>
2021-03-02 09:12:43 +0000

    - Updated to use newer Sutil (0.2.1-alpha) - Updated to use Feliz.Engine (currrently shipping as part of Sutil for now)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,4 +2,6 @@
 
 ## Changelog (most recent first)
 
+- Updated to use newer Sutil (0.2.1-alpha)
+- Updated to use Feliz.Engine (currrently shipping as part of Sutil for now)
 - Remove transitive dependencies from fsproj
--- src/App/App.fs
+++ src/App/App.fs
@@ -3,14 +3,16 @@ module App
 open Sutil
 open Sutil.DOM
 open Sutil.Attr
+open Feliz
+open type Feliz.length
 
 let view() =
     Html.div [
         style [
             Css.fontFamily "Arial, Helvetica, sans-serif"
-            Css.textAlign "center"
-            Css.marginTop "40px"
-            Css.fontSize "10ex"
+            Css.textAlignCenter
+            Css.marginTop (px 40)
+            Css.fontSize (ex 10)
         ]
         text "Hello World"
     ]
